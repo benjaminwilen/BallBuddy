9b2e487d0441af60bfbfa42d1d0d515254b7994f
Benjamin Wilen <benjaminwilen@gmail.com>
2023-03-14 14:30:32 -0400

    Working on ARReciever

---
+++ Ball Buddy V2/ARReceiver.swift
@@ -0,0 +1,60 @@
+//
+//  ARReceiver.swift
+//  Ball Buddy V2
+//
+//  Created by Benjamin Wilen on 3/14/23.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+import ARKit
+
+// Receive the newest AR data from an `ARReceiver`.
+protocol ARDataReceiver: AnyObject {
+    func onNewARData(arData: ARData)
+}
+
+//- Tag: ARData
+// Store depth-related AR data.
+final class ARData {
+    var depthImage: CVPixelBuffer?
+    var confidenceImage: CVPixelBuffer?
+}
+
+// Configure and run an AR session to provide the app with depth-related AR data.
+final class ARReceiver: NSObject, ARSessionDelegate {
+    var arData = ARData()
+    var arSession = ARSession()
+    weak var delegate: ARDataReceiver?
+    
+    // Configure and start the ARSession.
+    override init() {
+        super.init()
+        arSession.delegate = self
+        start()
+    }
+    
+    // Configure the ARKit session.
+    func start() {
+        guard ARWorldTrackingConfiguration.supportsFrameSemantics(.sceneDepth) else { return }
+        // Enable both the `sceneDepth` and `smoothedSceneDepth` frame semantics.
+        let config = ARWorldTrackingConfiguration()
+        config.frameSemantics = .sceneDepth
+        arSession.run(config)
+    }
+    
+    func pause() {
+        arSession.pause()
+    }
+  
+    // Send required data from `ARFrame` to the delegate class via the `onNewARData` callback.
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        if (frame.sceneDepth != nil) {
+            arData.depthImage = frame.sceneDepth?.depthMap
+            arData.confidenceImage = frame.sceneDepth?.confidenceMap
+            //delegate?.onNewARData(arData: arData)
+        }
+    }
+}
+
